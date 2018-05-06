d715592ebfcb33dae2ec56163602268d9afc1a19
Dan Mooney <dmooney@magento.com>
2018-05-06 10:50:43 -0500

    Add music script and audio track

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+	private AudioSource audio;
+	
+	void Start ()
+	{
+		AudioSource audio = GetComponent<AudioSource>();
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
