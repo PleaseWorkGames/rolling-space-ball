c4cc2826d7d6a373a8823aa274a70a35f9e568f4
Dan Mooney <dmooney@magento.com>
2018-05-06 19:49:35 -0500

    Welcome to the SPACE overlay mask jam

---
+++ Assets/Scripts/SpaceMask.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceMask : MonoBehaviour
+{
+	private Mask overlayMask;
+	private Text text;
+	
+	void Start ()
+	{
+		this.overlayMask = GetComponent<Mask>();
+		this.text = GetComponentInChildren<Text>();
+	}
+	
+	/**
+	 * TODO - Move overlay mask based on position of ball 
+	 */
+	void Update ()
+	{
+		Vector2 originalTextPosition = text.transform.position;
+		
+		overlayMask.transform.position = new Vector2(
+			overlayMask.transform.position.x + 1.0f,
+			overlayMask.transform.position.y
+		);
+
+		// reset position on child text (TODO - there's gotta be a better way to do this)
+		text.transform.position = originalTextPosition;
+	}
+}
