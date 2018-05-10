42b0b98e9868b0befe99e86f8ef97578d3340878
James Calcaben <jcalcaben+github@gmail.com>
2018-05-09 21:18:27 -0500

    Add space background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[
+	RequireComponent(typeof(SpriteRenderer))
+]
+public class Background : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>() as SpriteRenderer;
+
+		float worldScreenHeight = Camera.main.orthographicSize * 2.0F;
+		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+		spriteRenderer.size = new Vector2( worldScreenWidth, worldScreenHeight );
+
+		transform.position = new Vector2(0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
