92757524d0d28c8ec02ab872cf33aa1bce5493f4
Dan Mooney <dan.r.mooney@gmail.com>
2018-05-11 22:43:27 -0500

    Move overlay mask with respect to relativity between screen width and text width

--- Assets/Scripts/SpaceMask.cs
+++ Assets/Scripts/SpaceMask.cs
@@ -8,25 +8,53 @@ public class SpaceMask : MonoBehaviour
 	private Mask overlayMask;
 	private Text text;
 	
+	private float screenWidth;
+	private float textWidth;
+	private float originalPlayerPositionX;
+	private float originalOverlayMaskPositionX;
+
+	public Player player;
+	public Camera camera;
+	
 	void Start ()
 	{
 		this.overlayMask = GetComponent<Mask>();
 		this.text = GetComponentInChildren<Text>();
+		
+		this.originalPlayerPositionX = camera.WorldToScreenPoint(this.player.transform.position).x;
+		this.originalOverlayMaskPositionX = this.overlayMask.transform.localPosition.x;
+		
+		this.screenWidth = Screen.width;
+		this.textWidth = this.text.GetComponent<RectTransform>().rect.width;
 	}
 	
 	/**
-	 * TODO - Move overlay mask based on position of ball 
+	 * Move overlay mask based on position of ball 
 	 */
-	void Update ()
+	void FixedUpdate ()
 	{
-		Vector2 originalTextPosition = text.transform.position;
+		float currentPlayerPositionX = camera.WorldToScreenPoint(this.player.transform.position).x;
+		float differencePlayerPositionX = this.originalPlayerPositionX - currentPlayerPositionX;
+
+		float overlayMaskPositionXDestination = this.originalOverlayMaskPositionX - differencePlayerPositionX * ((textWidth / 2) / screenWidth); // TODO - figure out why -10 is left side of stage for player and overlay mask  
+		Debug.Log(overlayMaskPositionXDestination + "  " + differencePlayerPositionX);
 		
-		overlayMask.transform.position = new Vector2(
-			overlayMask.transform.position.x + 1.0f,
-			overlayMask.transform.position.y
+//		this.screenWidth;
+		
+		// move overlay mask to new position based on ball position.  this requires moving the mask and then moving the children back to original positions, since it moves both and I haven't looked into how to do this better
+		Vector3 originalTextPosition = text.transform.position;
+		
+		overlayMask.transform.localPosition = new Vector2(
+			overlayMaskPositionXDestination,
+			overlayMask.transform.localPosition.y
 		);
 
 		// reset position on child text (TODO - there's gotta be a better way to do this)
 		text.transform.position = originalTextPosition;
+		
+		player.transform.localPosition = new Vector2(
+			player.transform.position.x - 0.01f,
+			player.transform.position.y
+		);
 	}
 }
